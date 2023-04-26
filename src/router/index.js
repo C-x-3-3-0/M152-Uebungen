a599622c437f834f203712b3a09a635300844f42
Hao Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2023-04-26 15:43:27 +0200

    Aufgabe 12.2 done

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Page1 from '../views/Pages/Page1.vue'
+import Aufgabe1 from '../views/Pages/Aufgabe1.vue'
+import Aufgabe12_2 from '../views/Pages/Aufgabe12_2.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +9,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
       path: '/aufgabe1',
       name: 'Aufgabe 1',
-      component: Page1,
+      component: Aufgabe1,
+    },
+    {
+      path: '/aufgabe12_2',
+      name: 'Aufgabe 12.2',
+      component: Aufgabe12_2,
     }
   ]
 })
---
+++ src/views/Pages/Aufgabe12_2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <svg width="150" height="150">
+      <polygon points="50,5 95,85 5,85" stroke-miterlimit="10" stroke-linejoin="round" stroke-width="4"/>
+      <polygon points="50,105 95,30 5,30" stroke-miterlimit="10" stroke-linejoin="round" stroke-width="4" pointer-events="none" />
+    </svg>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  div {
+    display: flex;
+    justify-content: center;
+  }
+  polygon {
+    fill: #ffffff;
+    stroke: #0000ff;
+  }
+</style>
